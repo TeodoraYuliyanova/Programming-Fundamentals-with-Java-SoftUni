9b9376ff23ba0b40fc035eb12808338fadf8663c
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-06-03 20:50:45 +0100

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/src/PrintAndSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+
+        for (int i = num1; i <= num2; i++) {
+            sum += i;
+            System.out.print(i + " ");
+        }
+        System.out.printf("%nSum: %d" , sum);
+
+
+    }
+}
