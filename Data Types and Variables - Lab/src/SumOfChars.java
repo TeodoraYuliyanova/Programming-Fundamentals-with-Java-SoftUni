e9f3d53d19cda7930e2228cf6d1a842447661bd6
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-06-05 23:55:01 +0100

    Add files via upload

---
+++ Data Types and Variables - Lab/src/SumOfChars.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfLines = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+
+        for (int i = 0; i < numberOfLines; i++) {
+            char letters = scanner.nextLine().charAt(0);
+
+
+            sum += letters;
+        }
+        System.out.printf("The sum equals: %d", sum);
+
+    }
+}
