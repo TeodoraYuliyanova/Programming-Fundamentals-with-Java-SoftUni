7bf757270293590c621749648ed5b9a03ed014f9
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-06-21 19:37:51 +0100

    Add files via upload

---
+++ Methods - Lab/src/CalculateRectangleArea.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double width = Double.parseDouble(scanner.nextLine());
+        double length =  Double.parseDouble(scanner.nextLine());
+        double output = area(width, length);
+
+        System.out.printf("%.0f", output);
+
+
+
+    }
+
+
+    public static double area(double width, double length){
+        return width * length;
+    }
+}
