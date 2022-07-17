80115962199f661bcc2dc162e6cea15470cdc32b
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-07-17 20:25:47 +0100

    Add files via upload

---
+++ Associative Arrays - Lab/src/WordFilter.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] line = Arrays.stream(scanner.nextLine().split(" ")).filter(e -> e.length() % 2 == 0).toArray(String [] ::new);
+
+        System.out.println(String.join(System.lineSeparator(), line));
+    }
+}
