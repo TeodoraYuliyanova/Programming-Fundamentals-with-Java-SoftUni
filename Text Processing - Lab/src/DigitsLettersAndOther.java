a10e436422749115f56bc09997af7367e3db7995
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2022-07-24 22:11:11 +0100

    Add files via upload

---
+++ Text Processing - Lab/src/DigitsLettersAndOther.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DigitsLettersAndOther {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String text = scanner.nextLine();
+
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder other = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char symbol = text.charAt(i);
+
+            if (Character.isDigit(symbol)){
+
+                digits.append(symbol);
+
+            }else if(Character.isLetter(symbol)){
+
+                letters.append(symbol);
+            }else {
+                other.append(symbol);
+            }
+
+        }
+
+        System.out.println(digits);
+        System.out.println(letters);
+        System.out.println(other);
+    }
+}
